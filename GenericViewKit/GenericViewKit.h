bfe3ce20d77591e6b378927b63e087a77a16185f
Kristian Andersen <hello@kristian.co>
2016-05-27 11:29:06 +0200

    Initial Commit

---
+++ GenericViewKit/GenericViewKit.h
@@ -0,0 +1,19 @@
+//
+//  GenericViewKit.h
+//  GenericViewKit
+//
+//  Created by Kristian Andersen on 27/05/16.
+//  Copyright © 2016 Kristian Andersen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GenericViewKit.
+FOUNDATION_EXPORT double GenericViewKitVersionNumber;
+
+//! Project version string for GenericViewKit.
+FOUNDATION_EXPORT const unsigned char GenericViewKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GenericViewKit/PublicHeader.h>
+
+
