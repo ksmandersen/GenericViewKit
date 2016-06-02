67d01d86ecf19572b626e07275a1caf7cdfaa014
Kristian Andersen <hello@kristian.co>
2016-06-02 10:51:40 +0200

    Added tvOS target

---
+++ GenericViewKit tvOS/GenericViewKit tvOS.h
@@ -0,0 +1,19 @@
+//
+//  GenericViewKit tvOS.h
+//  GenericViewKit tvOS
+//
+//  Created by Kristian Andersen on 02/06/16.
+//  Copyright © 2016 Kristian Andersen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GenericViewKit tvOS.
+FOUNDATION_EXPORT double GenericViewKit_tvOSVersionNumber;
+
+//! Project version string for GenericViewKit tvOS.
+FOUNDATION_EXPORT const unsigned char GenericViewKit_tvOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GenericViewKit_tvOS/PublicHeader.h>
+
+
